fbe0bb78c95be612d5e5eb7a1c3208b092505cff
Kenix3 <chris.deckard3@gmail.com>
2018-06-13 17:43:05 -0400

    Update DataConversions.java
    Supporting multiple colour codes from user messages.

--- server/src/org/rscemulation/server/util/DataConversions.java
+++ server/src/org/rscemulation/server/util/DataConversions.java
@@ -223,8 +223,9 @@ public final class DataConversions {
 			boolean flag = true;
 			for(int l1 = 0; l1 < k; l1++) {
 				char c = buffer[l1];
-				if (l1 > 4 && c == '@')
-					buffer[l1] = ' ';
+ 				// Don't blank out further @ ... most recent official client supports multiple colour codes from user messages 
+				/*if (l1 > 4 && c == '@')
+					buffer[l1] = ' ';*/
 				if (c == '%')
 					buffer[l1] = ' ';
 				if (flag && c >= 'a' && c <= 'z') {
